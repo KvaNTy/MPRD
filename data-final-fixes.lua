15206205ff646a2919ccc7e9c7007e8bbfd29dd6
KvaNTy Troiden <kvanty.troiden@gmail.com>
2018-07-21 07:19:54 +0300

    Settings and Probability handling (#3)

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,48 +1,86 @@
-local function get_products_of_type(results, target_type)
-	local product_keys_list = {}
-	for _, product in pairs(results) do
-		local product_type = ""
-		local product_name = ""
-		local product_key = ""
-		if product.type ~= nil then -- {type="item|fluid", name="product_name", ammount=int}
-			if product.type == "item" and product.type == target_type then 
-				product_key = "item-name." .. product.name
-			elseif product.type == "fluid" and product.type == target_type then 
-				product_key = "fluid-name." .. product.name
-			end
-		else
-			for i, value in pairs(product) do -- {[1]="item|fluid", [2]="product_name", [3]=int}
-				if type(value) == "string" then
-					if value == "item" or value == "fluid" then product_type = value
-					else product_name = value end
-				end
-			end
-			if product_type == "item" and product_name ~= "" and product_type == target_type then 
-				product_key = "item-name." .. product_name
-			elseif product_type == "fluid" and product_name ~= "" and product_type == target_type then 
-				product_key = "fluid-name." .. product_name
-			end
-		end
-		if product_key ~= "" then table.insert(product_keys_list, product_key) end
+local Vertical_Display = settings.startup["mprd-vertical-display"].value
+local Amount_Display = settings.startup["mprd-amount-display"].value
+
+-- Result definition structure can vary:
+-- {name="product_name", amount=int}
+-- {name="product_name", amount=int, probability=int}
+-- {type="item|fluid", name="product_name", amount=int}
+-- {type="item|fluid", name="product_name", amount=int, probability=int}
+-- {[1]="item|fluid", [2]="product_name", [3]=int, [4]=int}
+local function parse_product_info(result)
+	local p_type = nil
+	local p_name = nil
+	local p_amount = nil
+	local p_probability = nil
+
+	-- Get Type
+	if result.type ~= nil then p_type = result.type
+	else
+		-- {[1]="item|fluid", [2]="product_name", ... }
+		if type(result[1]) == "string" and type(result[2]) == "string" then	p_type = result[1] end
 	end
-	return product_keys_list
+
+	-- Get Name
+	if result.name ~= nil then p_name = result.name
+	else
+		if p_type ~= nil then p_name = result[2]	-- {[1]="item|fluid", [2]="product_name", [3]=int}
+		else p_name = result[1]						-- {[1]="product_name", [2]=int}
+		end
+	end
+
+	-- Get Amount
+	if result.amount ~= nil then p_amount = result.amount
+	else
+		if p_type ~= nil then p_amount = result[3]	-- {[1]="item|fluid", [2]="product_name", [3]=int}
+		else p_amount = result[2]					-- {[1]="product_name", [2]=int}
+		end
+	end
+
+	-- Get Probability
+	if result.probability ~= nil then p_probability = result.probability
+	else
+		if p_type ~= nil then p_probability = result[4] -- {[1]="item|fluid", [2]="product_name", [3]=int, [4]=int}
+		else p_probability = result[3]					-- {[1]="product_name", [2]=int, [3]=int}
+		end
+	end
+
+	return {type=p_type, name=p_name, amount=p_amount, probability=p_probability}
+end
+
+local function format_line(product, name_prefix, output)
+	if Vertical_Display then 
+		table.insert(output, "\n")
+		table.insert(output, {name_prefix .. product.name})
+	else
+		table.insert(output, ", ")
+		table.insert(output, {name_prefix .. product.name})
+	end
+	if Amount_Display and product.amount ~= nil then
+		if product.probability ~= nil and product.probability ~= 1 then
+			table.insert(output, " [" .. product.amount .. "*" .. product.probability * 100 .. "%]")
+		else
+			table.insert(output, " [" .. product.amount .. "]")
+		end
+	end
+	return output
 end
 
 local function generate_description(recipe, results)
 	local new_description = {'', {"recipe-description.mprd-caption"}, " "}
-	local items = get_products_of_type(results, "item")
-	local fluids = get_products_of_type(results, "fluid")
 
-	for _, item_key in pairs(items) do -- Items must be listed first
-		table.insert(new_description, ", ")
-		table.insert(new_description, {item_key})
+	local fluids = {}
+	for _, result in pairs(results) do
+		local product = parse_product_info(result)
+		-- If type is not defined product supposed to be an Item
+		if product.type == "item" or product.type == nil then
+			new_description = format_line(product, "item-name.", new_description)
+		else
+			fluids = format_line(product, "fluid-name.", fluids)
+		end
 	end
-	for _, fluid_key in pairs(fluids) do
-		table.insert(new_description, ", ")
-		table.insert(new_description, {fluid_key})
-	end
+	for _, key in pairs(fluids) do table.insert(new_description, key) end -- Items first, Fluids last
 
-	table.remove(new_description, 4) -- Remove preemptive first comma
+	if not Vertical_Display then table.remove(new_description, 4) end -- Remove preemptive first comma
 	if recipe.localised_description or recipe.description then -- Preserve existing description
 		if recipe.localised_description then table.insert(new_description, 2, recipe.localised_description)
 		else table.insert(new_description, 2, {recipe.description}) end
